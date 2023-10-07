f121f580106cf05c96596d80c97e9018474d4aff
arwin <arwin@LEGION>
2023-10-06 22:27:51 -0400

    Add project files.

---
+++ Stonks/Models/aCandlestick.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stonks.Models
+{
+    internal class aCandlestick
+    {
+        public Decimal open { get; set; }
+        public Decimal high { get; set; }
+        public Decimal low { get; set; }
+        public Decimal close { get; set; }
+        public long volume { get; set; }
+
+        public DateTime date { get; set; }
+
+        public aCandlestick()
+        {
+            
+        }
+
+        public aCandlestick(string[] values)
+        {
+            
+        }
+    }
+}
