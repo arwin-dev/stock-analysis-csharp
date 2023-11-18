5c725ddb6998d7b020bd15fd7c1bb51c375cb1d8
arwin-dev <arwin.dev@outlook.com>
2023-11-18 10:51:36 -0500

    recog

---
+++ Stonks/Recognizers/Recognizer.cs
@@ -0,0 +1,66 @@
+﻿using Stonks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stonks.Recognizers
+{
+    abstract class Recognizer
+    {
+        public int patternSize;
+        public string patternName;
+        public Recognizer(int patternSize, string patternName)
+        {
+            this.patternSize = patternSize;
+
+            this.patternName = patternName;
+        }
+        public List<smartCandlestick> Recognize(List<smartCandlestick> smartCandlesticks)
+        {
+            List<smartCandlestick> smartCandlesticksTemp = new List<smartCandlestick>();
+            foreach (smartCandlestick cs in smartCandlesticks)
+            {
+                if(recognizePattern(cs)) 
+                {
+                    smartCandlesticksTemp.Add(cs);
+                }
+            }
+            return smartCandlesticksTemp;
+        }
+
+        public virtual bool recognizePattern(List<smartCandlestick> sc)
+        {
+            return false;
+        }
+    }
+
+    class dojiRecognizer : Recognizer
+    {
+        public dojiRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
+        {
+            
+        }
+        public override bool recognizePattern(List<smartCandlestick> smartCandlesticks)
+        {
+            if(patternSize == 3)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class hammerRecognizer : Recognizer
+    {
+        public hammerRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
+        {
+            
+        }
+        public override bool recognizePattern(List<smartCandlestick> smartCandlesticks)
+        {
+            return smartCandlesticks[0].isHammer;
+        }
+    }
+}
