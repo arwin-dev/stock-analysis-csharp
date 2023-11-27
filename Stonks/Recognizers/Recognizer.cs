861b0b090e65f350d20b015720308f4a7ffb2d28
arwin-dev <arwin.dev@outlook.com>
2023-11-26 20:17:19 -0500

    done

--- Stonks/Recognizers/Recognizer.cs
+++ Stonks/Recognizers/Recognizer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace Stonks.Recognizers
 {
+    /// <summary>
+    /// Abstract class representing a generic pattern recognizer
+    /// </summary>
     abstract class Recognizer
     {
         public int patternSize;
@@ -13,53 +16,65 @@ namespace Stonks.Recognizers
 
             this.patternName = patternName;
         }
-        public List<smartCandlestick> Recognize(List<smartCandlestick> smartCandlesticks)
-        {
-            List<smartCandlestick> smartCandlesticksTemp = new List<smartCandlestick>();
-            foreach (smartCandlestick cs in smartCandlesticks)
-            {
-                if(recognizePattern(cs)) 
-                {
-                    smartCandlesticksTemp.Add(cs);
-                }
-            }
-            return smartCandlesticksTemp;
-        }
 
+        /// <summary>
+        /// Virtual method to be implemented by derived classes for pattern recognition of a list of smartCandlesticks
+        /// </summary>
+        /// <param name="sc">A smartCandlestick</param>
+        /// <returns></returns>
         public virtual bool recognizePattern(smartCandlestick sc)
         {
             return false;
         }
 
+        /// <summary>
+        /// Virtual method to be implemented by derived classes for pattern recognition of a list of smartCandlesticks
+        /// </summary>
+        /// <param name="sc">A list of smartCandlesticks</param>
+        /// <returns></returns>
         public virtual bool recognizePattern(List<smartCandlestick> sc)
         {
             return false;
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Doji candlesticks
+    /// </summary>
     class dojiRecognizer : Recognizer
     {
         public dojiRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
         {
             
         }
+
+        // Override of the recognizePattern method for a single smartCandlestick
         public override bool recognizePattern(smartCandlestick cs)
         {
             return cs.isDoji;
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Hammer candlesticks
+    /// </summary>
     class hammerRecognizer : Recognizer
     {
         public hammerRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
         {
             
         }
+
+        // Override of the recognizePattern method for a single smartCandlestick
         public override bool recognizePattern(smartCandlestick cs)
         {
             return cs.isHammer;
         }
     }
+
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting bullish candlesticks
+    /// </summary>
     class bullishRecognizer : Recognizer
     {
         public bullishRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -72,6 +87,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting bearish candlesticks
+    /// </summary>
     class bearishRecognizer : Recognizer
     {
         public bearishRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -84,6 +102,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting neutral candlesticks
+    /// </summary>
     class neutralRecognizer : Recognizer
     {
         public neutralRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -96,6 +117,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting marubozu candlesticks
+    /// </summary>
     class marubozuRecognizer : Recognizer
     {
         public marubozuRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -108,6 +132,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Dragonfly Doji candlesticks
+    /// </summary>
     class dragonflyDojiRecognizer : Recognizer
     {
         public dragonflyDojiRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -120,6 +147,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Gravestone Doji candlesticks
+    /// </summary>
     class gravestoneDojiRecognizer : Recognizer
     {
         public gravestoneDojiRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -132,6 +162,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Inverted Hammer candlesticks
+    /// </summary>
     class invertedHammerRecognizer : Recognizer
     {
         public invertedHammerRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -144,6 +177,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting peak candlesticks
+    /// </summary>
     class peakRecognizer : Recognizer
     {
         public peakRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -151,6 +187,7 @@ namespace Stonks.Recognizers
             
         }
 
+        // Override of the recognizePattern method for multi smartCandlestick
         public override bool recognizePattern(List<smartCandlestick> sc)
         {
             if (sc.Count == 3) 
@@ -166,6 +203,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting valley candlesticks
+    /// </summary>
     class valleyRecognizer : Recognizer
     {
         public valleyRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -188,6 +228,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Bullish Engulfing candlesticks
+    /// </summary>
     class bullishEngulfingRecognizer : Recognizer
     {
         public bullishEngulfingRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -195,6 +238,7 @@ namespace Stonks.Recognizers
 
         }
 
+        // Override of the recognizePattern method for multi smartCandlestick
         public override bool recognizePattern(List<smartCandlestick> sc)
         {
             if (sc.Count == patternSize)
@@ -209,6 +253,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Bearish Engulfing candlesticks
+    /// </summary>
     class bearishEngulfingRecognizer : Recognizer
     {
         public bearishEngulfingRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -230,6 +277,9 @@ namespace Stonks.Recognizers
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Bullish Harami candlesticks
+    /// </summary>
     class bullishHaramiRecognizer : Recognizer
     {
         public bullishHaramiRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -244,13 +294,16 @@ namespace Stonks.Recognizers
                 smartCandlestick sc1 = sc[0];
                 smartCandlestick sc2 = sc[1];
 
-                return (sc1.isBearish && sc2.bodyRange < sc1.bodyRange && sc2.range < sc1.range && sc1.close < sc2.open);
+                return ((sc2.close > sc2.open) && (sc1.close < sc1.open) && (sc2.close < sc1.open) && (sc2.open > sc1.close));
             }
 
             return false;
         }
     }
 
+    /// <summary>
+    /// Class representing a specific pattern recognizer for detecting Bearish Harami candlesticks
+    /// </summary>
     class bearishHaramiRecognizer : Recognizer
     {
         public bearishHaramiRecognizer(int patternSize, string patternName) : base(patternSize, patternName)
@@ -265,7 +318,7 @@ namespace Stonks.Recognizers
                 smartCandlestick sc1 = sc[0];
                 smartCandlestick sc2 = sc[1];
 
-                return sc1.isBullish && sc2.bodyRange < sc1.bodyRange && sc2.range < sc1.range && sc1.close > sc2.open;
+                return ((sc2.close < sc2.open) && (sc1.close > sc1.open) && (sc2.close > sc1.open) && (sc2.open < sc1.close));
             }
 
             return false;
